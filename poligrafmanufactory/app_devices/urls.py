2163af9736fe9828a8cebd17cabf69c3740bba2f
Norvint <Kogleser@gmail.com>
2022-08-05 14:45:09 +0300

    added devices list

--- poligrafmanufactory/app_devices/urls.py
+++ poligrafmanufactory/app_devices/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = []
+from app_devices.views import InterfaceAdapterListView, DevicesListView
+
+urlpatterns = [
+    path('', DevicesListView.as_view(), name='devices_list'),
+    path('adapters/', InterfaceAdapterListView.as_view(), name='adapters_list')
+]
--- poligrafmanufactory/app_devices/views.py
+++ poligrafmanufactory/app_devices/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from app_devices.models import InterfaceAdapter, Device
+
+
+class InterfaceAdapterListView(ListView):
+    model = InterfaceAdapter
+    context_object_name = 'adapters'
+    template_name = 'app_devices/adapters_list.html'
+
+
+class DevicesListView(ListView):
+    model = Device
+    context_object_name = 'devices'
+    template_name = 'app_devices/devices_list.html'
